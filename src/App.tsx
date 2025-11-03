6b4283b6a5a886cced6e1007621b7a60bebf5fea
ldevesa <ldevesa@latcom.com>
2025-11-03 10:19:35 -0300

    Mi primer commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>¡Mi Prueba con Tauri!</h1>
+      <p>Esto es React corriendo dentro de una app de escritorio.</p>
+    </div>
+  );
+}
+
+export default App;
